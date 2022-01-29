2420121ffdcf74f027f679fa7b74e814c4e5eb12
UssefBhl <ybhl83@gmail.com>
2022-01-29 14:33:11 +0100

    ✨ added returnMessageAsResponse function

--- src/functions/index.js
+++ src/functions/index.js
@@ -2,6 +2,11 @@ const returnErrorAsResponse = (res,error) => {
     res.status(400).json({error});
 }
 
+const returnMessageAsResponse = (res,message) => {
+    res.status(200).json({message});
+}
+
 module.exports = {
-    returnErrorAsResponse
+    returnErrorAsResponse,
+    returnMessageAsResponse
 }

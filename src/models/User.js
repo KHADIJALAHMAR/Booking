d8480fb7322cc9560b44c0ed8e112ebd11bb6bf9
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2022-02-01 11:58:38 +0100

    addAuthorizationsInOwnerRoutes

--- src/models/User.js
+++ src/models/User.js
@@ -1,53 +1,52 @@
 const mongoose = require("mongoose");
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
 const RoleSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      required: true
-    },
-    status: {
-      type: Boolean
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: Boolean,
+  },
 });
 
 // create Schema
 const UserSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: true
+    required: true,
   },
   email: {
     type: String,
-    required: true
+    required: true,
   },
   password: {
     type: String,
-    required: true
+    required: true,
   },
   gender: {
     type: String,
-    required: true
+    required: true,
   },
   role: RoleSchema,
-
 });
 
 // methods
-UserSchema.pre('save', function(next) {
+UserSchema.pre("save", function (next) {
   const user = this;
   // only hash the password if it has been modified (or is new)
-  if (!user.isModified('password')) return next();
+  if (!user.isModified("password")) return next();
 
   bcrypt.hash(user.password, 10).then((hashedPass) => {
-      user.password = hashedPass;
-      next();
+    user.password = hashedPass;
+    next();
   });
 });
 
-UserSchema.methods.comparePasswords = function(password) {
+UserSchema.methods.comparePasswords = function (password) {
   return bcrypt.compare(password, this.password);
-}
+};
 
 const User = mongoose.model("User", UserSchema);
 

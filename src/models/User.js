ee8a14269337b4531f84a1fded5e4512fb27648f
UssefBhl <ybhl83@gmail.com>
2022-02-01 12:08:16 +0100

    ✨ added validation in Schema

--- src/models/User.js
+++ src/models/User.js
@@ -4,7 +4,8 @@ const bcrypt = require('bcryptjs');
 const RoleSchema = new mongoose.Schema({
     name: {
       type: String,
-      required: true
+      required: true,
+      enum: ['admin', 'customer', 'owner']
     },
     status: {
       type: Boolean
@@ -15,19 +16,25 @@ const RoleSchema = new mongoose.Schema({
 const UserSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: true
+    required: true,
+    minlength: [5, 'the username must be greater than 5 characters'],
+    maxlength: [20, 'the username must be less than 20 characters']
   },
   email: {
     type: String,
-    required: true
+    required: true,
+    match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'invalid email format']
   },
   password: {
     type: String,
-    required: true
+    required: true,
+    minlength: [5, 'the password must be greater than 5 characters'],
+    maxlength: [20, 'the password must be less than 20 characters']
   },
   gender: {
     type: String,
-    required: true
+    required: true,
+    enum: ['male', 'female']
   },
   role: RoleSchema,
 

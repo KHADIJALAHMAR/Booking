a9879526fa71f6befffd20cc94737231e431be8b
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2022-02-01 15:23:01 +0100

    deleteOwnerByAdmin

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,36 +1,26 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // reuiring Authorization By Roles Middleware
-const {authorizeWithRole} = require('../middlewares/authorizeUser')
+const { authorizeWithRole } = require("../middlewares/authorizeUser");
 
 // requiring controllers
-const Admins = require('../controllers/Admins');
+const Admins = require("../controllers/Admins");
 
 //  Routes
 
 router
-.route('/owner/accept')
-.put( authorizeWithRole('admin'), Admins.acceptOwner )
+  .route("/owner/accept")
+  .put(authorizeWithRole("admin"), Admins.acceptOwner);
 
-router
-.route('/owner/refuse')
-.put( Admins.refuseOwner )
+router.route("/owner/refuse").put(Admins.refuseOwner);
 
-router
-.route('/user/ban')
-.put( Admins.banUser )
+router.route("/user/ban").put(Admins.banUser);
 
-router
-.route('/user/unban')
-.put( Admins.unbanUser )
+router.route("/user/unban").put(Admins.unbanUser);
 
-router
-.route('/hotel/accept')
-.put( Admins.acceptHotel )
+router.route("/hotel/accept").put(Admins.acceptHotel);
 
-router
-.route('/hotel/refuse')
-.put( Admins.refuseHotel )
+router.route("/hotel/refuse").put(Admins.refuseHotel);
 
 module.exports = router;

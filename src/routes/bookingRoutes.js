1ae3839f97c4acd74d1fdce68f369b871a220db4
khadija <khadijalahmar8@gmail.com>
2022-02-01 09:27:57 +0100

    modification route booking

--- src/routes/bookingRoutes.js
+++ src/routes/bookingRoutes.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const { get } = require('express/lib/response');
 const router = express.Router();
 const ControllerBooking =require('../controllers/Bookings');
 
 
 
-router.route("/bookings")
+router.route("/")
 get(ControllerBooking.getBookings);
 delete(ControllerBooking.deleteBooking);
 

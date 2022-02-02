3ba36a4194dc44a980640d47c1b069ec91c0e616
UssefBhl <ybhl83@gmail.com>
2022-02-02 14:17:30 +0100

    ✨ added multer

--- src/routes/ownerRoutes.js
+++ src/routes/ownerRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const path = require('path');
 const multer = require('multer');
-// const upload = multer({ dest : })
+const upload = multer({ dest : path.join(__dirname, 'src' , 'public' , 'assets' , 'uploads' , 'images' , 'room_images')})
 
 // controller
 const Owners = require("../controllers/Owners");

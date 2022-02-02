65bf9255a3c3d48a7d618c2919a7157eae7e78f6
khadija <khadijalahmar8@gmail.com>
2022-02-02 10:15:54 +0100

    💄 add validation Room Type

--- src/models/RoomType.js
+++ src/models/RoomType.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const RoomTypeSchema = new mongoose.Schema({
   name: {
     type: String,
+    minlength:[6 ,'should be atleast of 6 length'],
+    maxlength:[26 , 'should not be more than 36 length']
   },
 });
 

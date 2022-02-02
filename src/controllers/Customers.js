b1e55ae0b1f8038e28d190716a8a08747b31b3cf
khadija <khadijalahmar8@gmail.com>
2022-02-02 10:14:50 +0100

    💄 add function controller Gget customers and add Booking

--- src/controllers/Customers.js
+++ src/controllers/Customers.js
@@ -1,17 +1,83 @@
-const {User} =require('../models'); 
-
-
-
+const {User, Booking} =require('../models'); 
+const express = require("express");
 
 
 
 const getCustomers = (req, res) =>{
-    User.find( function (err, user) {
-       console.log(user);
-    });
+    {
+        User.find({
+        where:{
+            role:"customer",
+        }
+        }).then(() => {
+        res.json()
+        })
+    }
+}
+
+
+
+
+const createBooking  = (req ,res) => {
+
+    const booking = {
+        date_from :  req.body.date_from ,
+        date_to:  req.body.date_to,
+        total_price: req.body.total_price ,
+        status: req.body.status,
+        paid:  req.body.paid,
+        // id_user :req.body.id_user,
+    };
+    try {
+        (async ()=> { 
+            await new Booking ({
+                date_from :  booking.date_from ,
+                date_to:  booking.date_to,
+                total_price: booking.total_price ,
+                status: booking.status,
+                paid:  booking.paid,
+                // id_user :booking.id_user,
+            }).save();
+            res.status(201).json({
+                message: `created Booking`,
+            });
+        })();
+    }
+        catch (error) {
+            res.status(400).json(err);
+        }
+
+};
+
+
+
+
+const updateBooking = (req ,res) =>{
+
+}
+
+
+const payBooking = (req ,res ) =>{
+
+}
+
+
+const getBannedUsers = (req, res) =>{
+
+
+
+}
+
+
+
+
+
+module.exports = {
+    getCustomers,
+    createBooking ,
+    getBannedUsers,
+    updateBooking,
+    payBooking,
+
 
   }
-
-module.exports ={
-    getCustomers,
-}

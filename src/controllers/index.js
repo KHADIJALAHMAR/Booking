57dc01f6603efe4f73e88fe79ad7f1638ecb040e
UssefBhl <ybhl83@gmail.com>
2022-02-03 11:26:42 +0100

    🚧 added the filter by ext and mimeType

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -9,8 +9,27 @@ const Users = require('./Users');
 // requiring multer
 const path = require('path');
 const multer = require('multer');
-const roomUpload = multer({ dest : path.join(path.dirname(__dirname) , 'public' , 'assets' , 'uploads' , 'images' , 'room_images')})
-const hotelUpload = multer({ dest : path.join(path.dirname(__dirname) , 'public' , 'assets' , 'uploads' , 'images' , 'hotel_images')})
+
+// filter function
+const fFilter = (req, file, cb) =>{    
+    // Allowed ext
+     const filetypes = /jpeg|jpg|png/;
+  
+   // Check ext
+    const extname =  filetypes.test(path.extname(file.originalname).toLowerCase());
+   // Check mime
+   const mimetype = filetypes.test(file.mimetype);
+  
+   if(mimetype && extname){
+       return cb(null,true);
+   } else {
+       cb('Error: Images Only!');
+   }
+}
+
+const roomUpload = multer({  fileFilter: fFilter , dest : path.join(path.dirname(__dirname) , 'public' , 'assets' , 'uploads' , 'images' , 'room_images')})
+
+const hotelUpload = multer({ fileFilter: fFilter , dest : path.join(path.dirname(__dirname) , 'public' , 'assets' , 'uploads' , 'images' , 'hotel_images')})
 
 module.exports = {
     Admins,

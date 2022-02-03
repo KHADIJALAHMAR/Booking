9113c40857cbbe3c199fda485396d4288851136e
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2022-02-03 14:42:54 +0100

    gatRoomsByCustomersMethod

--- src/routes/ownerRoutes.js
+++ src/routes/ownerRoutes.js
@@ -1,33 +1,40 @@
 const express = require("express");
 const router = express.Router();
-const multer = require('multer');
+const multer = require("multer");
 // const upload = multer({ dest : })
-const {authorizeWithRole} = require('../middlewares/authorizeUser');
+const { authorizeWithRole } = require("../middlewares/authorizeUser");
 
 // controller
 const Owners = require("../controllers/Owners");
 
 // routes
-router.route("/accepted")
-    .get(authorizeWithRole("admin"), Owners.getAcceptedOwners);
+router
+  .route("/accepted")
+  .get(authorizeWithRole("admin"), Owners.getAcceptedOwners);
 
-router.route("/refused")
-    .get(authorizeWithRole("admin"), Owners.getRefusedOwners);
+router
+  .route("/refused")
+  .get(authorizeWithRole("admin"), Owners.getRefusedOwners);
 
-router.route("/banned")
-    .get(authorizeWithRole("admin"), Owners.getBannedOwners);
+router
+.route("/banned")
+.get(authorizeWithRole("admin"), Owners.getBannedOwners);
 
-router.route("/room")
-    .post(authorizeWithRole("owner"), Owners.createRoom);
+router
+.route("/room")
+.post(authorizeWithRole("owner"), Owners.createRoom);
 
-router.route("/room/:roomId")
-    .put(authorizeWithRole("owner"), Owners.updateRoom)
-    .delete(authorizeWithRole("owner"), Owners.deleteRoom);
+router
+  .route("/room/:roomId")
+  .put(authorizeWithRole("owner"), Owners.updateRoom)
+  .delete(authorizeWithRole("owner"), Owners.deleteRoom);
 
-router.route("/booking/accept")
-    .put(authorizeWithRole("owner"), Owners.acceptBooking);
+router
+  .route("/booking/accept")
+  .put(authorizeWithRole("owner"), Owners.acceptBooking);
 
-router.route("/booking/refuse")
-    .put(authorizeWithRole("owner"), Owners.refuseBooking);
+router
+  .route("/booking/refuse")
+  .put(authorizeWithRole("owner"), Owners.refuseBooking);
 
 module.exports = router;

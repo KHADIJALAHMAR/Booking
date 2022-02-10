2b4ad1b08fbdd18866f685e4b89963f51133b6ce
mohammed <mohammedkabli53@gmail.com>
2022-02-10 10:26:50 +0100

    ♻️ changing the route of hotel

--- src/routes/hotelRoutes.js
+++ src/routes/hotelRoutes.js
@@ -9,28 +9,40 @@ const { authorizeWithRole } = require('../middlewares/authorizeUser');
 
 
 
-// router.route("/:id") 
-// .get(authorizeWithRole('owner'),Hotels.getHotelsbyowner)
+ 
 
 router.route("/") 
 .get(authorizeWithRole('admin', 'owner'),Hotels.getHotels)
 .post(authorizeWithRole('admin', 'owner') ,Hotels.createHotel)
-.put( authorizeWithRole('admin', 'owner') ,Hotels.updateHotel)
-.delete( authorizeWithRole('admin', 'owner') ,Hotels.deleteHotel);
+
+
+router.route("/:id")
+.put(authorizeWithRole('admin', 'owner') ,Hotels.updateHotel)
+.delete(authorizeWithRole('admin', 'owner') ,Hotels.deleteHotel);
+
 
 router.route("/accepted") 
 .get(Admins.getAcceptedHotels);
 
+
 router.route("/refused")
 .get(Admins.getRefusedHotels);
 
-// router.route("/filterByName")
-// .post(Hotels.getHotelsByName);
 
-// router.route("/filterByCity")
-// .post(Hotels.getHotelsByCity);
+router.route("/filterByName")
+.post(Hotels.getHotelsByName);
+
+
+router.route("/filterByCity")   
+.post(Hotels.getHotelsByCity);
+
 
 // router.route("/filterByStars")
 // .post(Hotels.getHotelsByStars);
+
+
+// router.route("/search")
+// .post(Hotels.searchFilters);
+
 
 module.exports = router;

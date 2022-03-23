bc5611abf52734741b81b49d894af65c5adaa17a
Mohammed Kabli <76818640+mohammedkabli@users.noreply.github.com>
2022-03-23 11:27:00 +0100

    🚨 fix route getowners

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -11,7 +11,12 @@ const { Admins, Owners, Customers, Users } = require("../controllers");
 
 router.route("/owner/create").post(Admins.createUser);
 
-router.route("/owner/owners").get(authorizeWithRole("admin"), Owners.getOwners);
+// router.route("/owners").get( Owners.getOwners);
+router.route("/owner/owners")
+.get(
+  // authorizeWithRole("admin"),
+Owners.getOwners);
+
 
 router
   .route("/owner/update/:userId")

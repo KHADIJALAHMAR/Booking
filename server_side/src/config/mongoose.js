436aef5926b363f9ee3667a8c00b0cf6a9857844
Youssef Bouhalla <77012260+YoussefBouhalla@users.noreply.github.com>
2022-04-12 12:03:14 +0000

    Delete server_side directory

--- server_side/src/config/mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect(
-  "mongodb://localhost/bookingApp",
-  () => console.log("database connected successfully"),
-  (e) => console.log(e.message)
-);
-
-module.exports = mongoose;
